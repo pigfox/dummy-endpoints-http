49153e547a78649e99206107d757b3e5a3008862
Peter Sjolin <pigfoxeria@gmail.com>
2024-10-23 22:52:29 -0700

    wip

--- client/main.go
+++ client/main.go
@@ -28,7 +28,7 @@ func main() {
 			defer wg.Done()
 
 			url := fmt.Sprintf("http://localhost:%d", port)
-			response, err := requester.MakeWG(url) // Create an empty structs.Response object and unmarshal
+			response, err := requester.MakeWG(url) // Assuming MakeWG returns a *structs.Response
 			if err != nil {
 				log.Printf("Error for port %d: %v", port, err)
 				return
@@ -36,7 +36,7 @@ func main() {
 
 			// Lock before modifying the shared slice
 			mu.Lock()
-			allResponses = append(allResponses, *response)
+			allResponses = append(allResponses, *response) // Append the dereferenced response
 			mu.Unlock()
 		}(port)
 	}
@@ -45,53 +45,49 @@ func main() {
 	wg.Wait()
 
 	// Group responses by address for price comparison
-	groupedByAddress := make(map[string][]structs.ResponseRow)
+	groupedByAddress := make(map[string][]structs.Response)
 
 	for _, response := range allResponses {
 		for _, resp := range response.Responses { // Access nested responses
-			groupedByAddress[resp.Address] = append(groupedByAddress[resp.Address], resp)
+			groupedByAddress[resp.Address] = append(groupedByAddress[resp.Address], response)
 		}
 	}
 
 	// Compare prices by address and check for threshold
 	priceDifferenceThreshold := structs.PriceDifferencePct / 100.0
 
-	for address, responseRows := range groupedByAddress {
-		if len(responseRows) < 2 {
-			continue // Not enough responses
+	for address, responses := range groupedByAddress {
+		if len(responses) < 2 {
+			continue // Not enough responses to compare
 		}
 
-		// Sort response rows by price within the group
-		sort.Slice(responseRows, func(i, j int) bool {
-			return responseRows[i].Price < responseRows[j].Price
+		// Sort responses by price within the group
+		sort.Slice(responses, func(i, j int) bool {
+			return responses[i].Responses[0].Price < responses[j].Responses[0].Price
 		})
 
 		// Compare prices across all DEXes for this address
-		for i := 0; i < len(responseRows); i++ {
-			for j := i + 1; j < len(responseRows); j++ {
-				price1 := float64(responseRows[i].Price)
-				price2 := float64(responseRows[j].Price)
+		for i := 0; i < len(responses); i++ {
+			for j := i + 1; j < len(responses); j++ {
+				price1 := float64(responses[i].Responses[0].Price)
+				price2 := float64(responses[j].Responses[0].Price)
 				diffPct := math.Abs(price2-price1) / price1
 
 				if diffPct > priceDifferenceThreshold {
 					// Log the price difference with DEX names
-					fmt.Printf("Price difference found for Address: %s\n", address)
-					var fromAddress, toAddress string
-					var lowPrice, highPrice int
-					if responseRows[i].Price < responseRows[j].Price {
-						fromAddress = responseRows[i].Address
-						toAddress = responseRows[j].Address
-						lowPrice = responseRows[i].Price
-						highPrice = responseRows[j].Price
+					var fromDex, toDex string
+					if responses[i].Responses[0].Price < responses[j].Responses[0].Price {
+						fromDex = responses[i].Dex
+						toDex = responses[j].Dex
 					} else {
-						fromAddress = responseRows[j].Address
-						toAddress = responseRows[i].Address
-						lowPrice = responseRows[j].Price
-						highPrice = responseRows[i].Price
+						fromDex = responses[j].Dex
+						toDex = responses[i].Dex
 					}
-					//Need to print the address of the DEXes, from Response.Dex <---------
-					fmt.Printf("Lowest Price: %s (Address: %s) ---> Highest Price: %s (Address: %s)\n", lowPrice, fromAddress, highPrice, toAddress)
-					fmt.Printf("Price1: %.2f, Price2: %.2f, Difference: %.2f%%\n", price1, price2, diffPct*100)
+
+					fmt.Printf("Price difference found for Address: %s\n", address)
+					fmt.Printf("Lowest Price: %s ---> Highest Price: %s\n", fromDex, toDex)
+					fmt.Printf("Price1: %.2f, Price2: %.2f, Difference: %.2f%%\n",
+						price1, price2, diffPct*100)
 				}
 			}
 		}
@@ -99,5 +95,5 @@ func main() {
 
 	fmt.Println("Total time taken: ", time.Since(beginTime))
 	fmt.Println("Total number of responses: ", len(allResponses))
-	fmt.Println("Total number of ports(servers): ", endPort-beginPort+1)
+	fmt.Println("Total number of ports (servers): ", endPort-beginPort+1)
 }
